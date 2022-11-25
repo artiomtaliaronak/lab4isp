a42e780c610585d53da2f5ee4657ca6b25f17018
artiomtaliaronak <artiomtaliaronak@gmail.com>
2022-11-25 04:49:55 +0300

    finished cloud deployment on yandex cloud

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ntufj6f#)-7boy2fi@a=jfy%5-p)x*8zis&dgz(a%gue6g-rvd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
